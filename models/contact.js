3373e06644a057a6c5cb54eea6a4d4a9d9b65ab4
sohelhussain <sohelhussaing@gmail.com>
2024-07-13 09:13:12 +0530

    creating joi in mongoose

--- models/contact.js
+++ models/contact.js
@@ -1,34 +1,46 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const contactSchema = new mongoose.Schema(
   {
     firstname: {
-      type: String, 
-      minlength: 2, 
-      maxlength: 10, 
+      type: String,
+      minlength: 2,
+      maxlength: 10,
       required: true,
+      trim: true,
     },
     lastname: {
       type: String,
       minlength: 2,
       maxlength: 10,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
       minlength: 2,
-      maxlength: 50, 
+      maxlength: 50,
       required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
     },
     number: {
-      type: String, // Changed to String to allow storing phone numbers with country codes
-      minlength: 7, // Minimum length for a valid phone number
-      maxlength: 15, // Maximum length for a valid phone number
+      type: String,
+      minlength: 7,
+      maxlength: 15,
       required: true,
+      trim: true,
     },
     message: {
       type: String,
       required: true,
+      trim: true,
     },
   },
   {
@@ -36,4 +48,28 @@ const contactSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model('Contact', contactSchema); // Capitalize model name for convention
+function validateModel(data) {
+  const schema = Joi.object({
+    firstname: Joi.string().min(2).max(10).required().trim(),
+
+    lastname: Joi.string().min(2).max(10).required().trim(),
+
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .min(2)
+      .max(50)
+      .required()
+      .trim()
+      .pattern(new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)),
+
+    number: Joi.string().min(7).max(15).required().trim(),
+
+    message: Joi.string().required().trim(),
+  });
+
+  let { error } = schema.validate(data);
+  return error;
+}
+
+module.exports.contactModel = mongoose.model("Contact", contactSchema);
+module.exports.validateModel = validateModel;
