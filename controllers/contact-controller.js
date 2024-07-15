d80110ad89151abffbb7438ae722b1cdac58c5a2
sohelhussain <sohelhussaing@gmail.com>
2024-07-15 15:15:11 +0530

    message are not working after the adding expressSession

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -3,24 +3,27 @@ const { contactModel, validateModel } = require("../models/contact");
 // Send contact details to the owner
 module.exports.contactController = async (req, res, next) => {
   try {
-    let { fullname, email, number, city} = req.body;
+    let { fullname, email, number, city } = req.body;
     console.log(req.body);
 
     // Validate the input using Joi
-    const error  = validateModel({ fullname, email, number, city });
+    const error = validateModel({ fullname, email, number, city });
     console.log(error);
     if (error) {
-      req.flash('validationError', err.details.map(err => err.message));
+      req.flash(
+        "validationError",
+        err.details.map((err) => err.message)
+      );
       return res.status(400).json({ error: error.details[0].message });
     }
 
     // Check for existing email in the database
-  const existingContact = await contactModel.findOne({ email });
-  if (existingContact) {
-    const errorMessage = 'Email already exists';
-    req.flash('validationError', [errorMessage]);
-    return res.status(400).send({ error: errorMessage });
-  }
+    const existingContact = await contactModel.findOne({ email });
+    if (existingContact) {
+      const errorMessage = "Email already exists";
+      req.flash("validationError", [errorMessage]);
+      return res.status(400).send({ error: errorMessage });
+    }
 
     // Create a new contact entry in the database
     let contact = await contactModel.create({
@@ -36,6 +39,8 @@ module.exports.contactController = async (req, res, next) => {
   } catch (error) {
     // Log the error and send a response
     console.error("Error creating contact:", error);
-    res.status(500).json({ error: "An error occurred while creating the contact." });
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the contact." });
   }
 };
